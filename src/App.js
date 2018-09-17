e457ac71c1d925bbbdad62b823998c6b09abb72f
rachtikarn <5910110279@psu.ac.th>
2018-09-17 11:45:59 +0700

    Array

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
 
+const word = "Hello";
 class App extends Component {
  render() {
   return (
-  <div>
-  <CharacterCard value="h"/>
-  <CharacterCard value="i"/>
+  <div>{
+  Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)}
+
   </div>
   );
  }
