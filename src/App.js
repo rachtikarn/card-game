082d2098e6b8aadc5d3755b81b43d9520aefa901
rachtikarn <5910110279@psu.ac.th>
2018-09-17 11:24:58 +0700

    CharacterCard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
 import CharacterCard from './CharacterCard';
 
 class App extends Component {
  render() {
- return (
- <div>
- <CharacterCard value="h"/>
-<CharacterCard value="i"/>
- </div>
- );
+  return (
+  <div>
+  <CharacterCard value="h"/>
+  <CharacterCard value="i"/>
+  </div>
+  );
  }
 }
 
