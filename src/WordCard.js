05b27b42bf236f8a0ab8b27d38810242e5ec8cc7
rachtikarn <5910110279@psu.ac.th>
2018-09-21 01:33:35 +0700

    update2

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,41 +1,47 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
 import _ from 'lodash';
-
 const prepareStateFromWord = (given_word) => {
     let word = given_word.toUpperCase()
     let chars = _.shuffle(Array.from(word))
     return {
-    word,
-    chars,
-    attempt: 1,
-    guess: [],
-    completed: false
+        word,
+        chars,
+        attempt: 1,
+        guess: [],
+        completed: false
     }
-   }
+}
    
 class WordCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    }
     activationHandler = (c) => {
-        let guess = [...this.state.guess, c]
+        let guess = [this.state.guess]+c
         this.setState({guess})
-        if(guess.length == this.state.chars.length){
-        if(guess.join('').toString() == this.state.word){
-        this.setState({guess: [], completed: true})
-        }else{
-        this.setState({guess: [], attempt: this.state.attempt + 1})
+        if(guess.length === this.state.chars.length){
+            if(guess === this.state.word){
+                this.setState({guess: [], completed: true})
+            }else{
+                this.setState({guess: [], attempt: this.state.attempt + 1})
+            }
         }
-        }
-       }
+    }
        
     render() {
      return (
-     <div>
+     <div className="App">
 
-    { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler} />)}
+    { Array.from(this.state.chars).map((c, i) => <CharacterCard value = {c} key = {i} attempt={this.state.attempt}
+     activationHandler = {this.activationHandler}/>)}
+     <p>รอบที่ : {this.state.attempt}</p>
+     <p>{this.state.completed? "WINNER!!!" : ""}</p>
      </div>
-     );
+     )
      }
-     activationHandler = c => { console.log(`${c} has been activated.`)}
+     
     }
 
 export default WordCard;
